5f6828062d920db1629acf45df521a2c9dad1a59
z29275164 <zz29275164@gmail.com>
2018-05-17 10:42:30 +0800

    main

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int i,s;
+int HomeWork,Sum;
 int main(int argc, char *argv[]) 
 {
-	for(i=0;i<10;i++){
-		s=s+i;
+	for(HomeWork=0;HomeWork<10;HomeWork++){
+		Sum=Sum+HomeWork;
 	}
-	printf("s=%d",s);
+	printf("s=%d",Sum);
 	return 0;
 }
